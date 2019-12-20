1b5b4c02dac14494f68a1450d8302f544647f958
ChandraMoorthy DakshinaMoorthy <chandramd@chennai.smartek21.st21>
2019-12-21 01:24:49 +0530

    'finally'

---
+++ src/webparts/charts/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IChartsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ChartsWebPartStrings' {
+  const strings: IChartsWebPartStrings;
+  export = strings;
+}
